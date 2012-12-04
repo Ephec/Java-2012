752568eb3027846d9754b0c351ae5fc08f3627cf
Antoine Betas <antoine.betas@gmail.com>
2012-12-04 12:36:15 +0100

    Ajout du chrono
    source : http://files.codes-sources.com/fichier.aspx?id=40336

--- src/Chrono.java
+++ src/Chrono.java
@@ -1,6 +1,62 @@
-public class Chrono {
+// Source du dossier : http://files.codes-sources.com/fichier.aspx?id=40336
 
-	// Il faut trouver le moyen de faire un chronomètre de toute urgence. Ce
-	// chronomètre sera utilisé par la classe partie lors du premier clic
+public class Chrono implements Runnable
+{
+	private Thread chronometre;
+	private int    dixiemeseconde = 0;
+	boolean arret;//utilisation de cette variable pour l'arret du thread
+
+
+	public void start ()
+	{
+		//demarrage de notre thread
+		chronometre = new Thread (this);
+		chronometre.start ();
+	}
+
+
+	public void run()
+	{
+		try
+		{
+			arret = false;
+			chronometre = Thread.currentThread();
+			//continuer tand que le thread n'a pas ete arrete 
+			while (!arret)
+			{
+				dixiemeseconde++;
+				Thread.sleep (100);
+
+			}
+		}
+		catch (InterruptedException e) { 
+			System.out.println(e.getMessage());
+		}
+		catch(NullPointerException e1)
+		{
+			System.out.println(e1.getMessage());
+		}
+	}
+
+	//methode pour "l'arret" de notre thread
+	public void stop ()
+	{
+		arret = true;
+		if(chronometre != null)
+		{
+			chronometre.isInterrupted();
+		}
+	}
+	
+	//methode retournant notre temps sous forme de chaine de caracteres
+	public String resultat()
+	{
+		return dixiemeseconde / 36000
+        + ":" + (dixiemeseconde / 6000) % 6 + (dixiemeseconde / 600) % 10
+        + ":" + (dixiemeseconde / 100) % 6  + (dixiemeseconde / 10) % 10
+        + ":" + dixiemeseconde % 10;
+
+	}
+	
 
 }
