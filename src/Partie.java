847e0ea4cc542d5c558532777f8d4107485a73e4
Antoine Betas <antoine.betas@gmail.com>
2012-12-04 18:33:09 +0100

    Modif Partie et ajout de Score

--- src/Partie.java
+++ src/Partie.java
@@ -48,13 +48,8 @@ public class Partie {
 		plateau = new Plateau(nbLignes, nbCols, nbMines);
 		plateau.initCases();
 
-		// Ici il faudra mettre le premier clic et ensuite jouer
-		//
-		// Méthodes a ajouter
-		//
-		// - Gagner
-		// - Perdre
-		// - ...
+		// Après l'initialisation des cases aura lieu le premier clic dans
+		// l'interface graphique je suppose. Il faut qu'on place les mines après
 
 	}
 
@@ -91,9 +86,10 @@ public class Partie {
 	}
 
 	/**
-	 * Défini ce qu'une est partie gagnée, c'est une partie dans laquelle
-	 * restantes correspondent au nombre de mines. Les drapeaux ne comptent pas,
-	 * ce sont des indications pour le joueur.
+	 * Défini ce qu'une est partie gagnée, c'est une partie dans laquelle les
+	 * cases restantes correspondent au nombre de mines. Les drapeaux ne
+	 * comptent pas, ce sont des indications pour le joueur. A appeller après
+	 * avoir découvert les cases autour de la case séléctionnée.
 	 * 
 	 * @param nbMines
 	 * @param nbDrapeau
@@ -103,12 +99,29 @@ public class Partie {
 
 		// Si le nombre de mines correspond au nombre de case - le nombre de
 		// case découverte
-		if (plateau.getNbCaseDecou() == (plateau.getNbCases() - plateau.getNbCaseDecou())) {
+		if (plateau.getNbCaseDecou() == (plateau.getNbCases() - plateau
+				.getNbCaseDecou())) {
 			plateau.toutDecouvrir();
 			return true;
 		} else
 			return false;
 
+	}
+
+	/**
+	 * Retourne vrai si la partie est perdue, autrement dit si la case est minée
+	 * et a été découverte. Cette méthode sera appellée juste après la
+	 * découverture de la case et avant de découvrir autour
+	 * 
+	 * @param isMinee
+	 * @return perdu
+	 */
+	public boolean perdu(boolean isMinee) {
+
+		if (isMinee)
+			return true;
+		else
+			return false;
 	}
 
 	// Getters et Setters pour le nb de lignes, de collonnes et de mines
