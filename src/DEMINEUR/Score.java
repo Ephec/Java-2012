797227640c8e731f707978eb2e9731006dd0bc2a
Antoine Betas <antoine.betas@gmail.com>
2012-12-06 14:13:53 +0100

    Ajout de score et encore modif de gitignore

--- src/DEMINEUR/Score.java
+++ src/DEMINEUR/Score.java
@@ -2,4 +2,17 @@ package DEMINEUR;
 
 public class Score {
 
+	private String nom;
+	private double temps;
+	private int level;
+
+	// Ici la classe score, c'est facile, on défini ce qu'est un objet score. On
+	// appellera le constructeur du socre dans le démineur est c'est le démineur
+	// qui créera le "tableau" de score et le fichier de sauvegarde des scores
+	// en fonctions des méthodes de la classe.
+
+	public Score() {
+
+	}
+
 }
