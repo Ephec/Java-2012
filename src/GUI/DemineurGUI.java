bc363115ef2769c111f702451f24e56a4aaaeadd
Antoine Betas <antoine.betas@gmail.com>
2012-12-09 11:33:00 +0100

    Delete de la GUI inutile

--- src/GUI/DemineurGUI.java
+++
@@ -1,9 +0,0 @@
-package GUI;
-
-public class DemineurGUI {
-
-	// Normalement c'est ici que se lancera la GUI. On peut donc considérer que
-	// le démineur est la fenêtre donc elle mais sans le plateau et le menu code
-	// dedans. Ce seront des classes
-
-}
