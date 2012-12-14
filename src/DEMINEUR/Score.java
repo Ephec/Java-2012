904a17438b890d74b1cffe0e3dfe241d58bd2bbb
Antoine Betas <antoine.betas@gmail.com>
2012-12-14 11:56:13 +0100

    Essai : sauvegarde du fichier scores

--- src/DEMINEUR/Score.java
+++ src/DEMINEUR/Score.java
@@ -1,5 +1,13 @@
 package DEMINEUR;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import GUI.Fenetre;
 
 public class Score {
@@ -34,6 +42,40 @@ public class Score {
 
         return str;
     }
+    
+    public void sauverFichier(){
+		String chaine="";
+		String fichier ="fichiertexte.txt";
+		
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				System.out.println(ligne);
+				chaine+=ligne+"\n";
+			}
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	
+		//création ou ajout dans le fichier texte
+		try {
+			FileWriter fw = new FileWriter (fichier);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter fichierSortie = new PrintWriter (bw); 
+				fichierSortie.println (chaine+"\n 2e test !!"); 
+			fichierSortie.close();
+			System.out.println("Le fichier " + fichier + " a été créé!"); 
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}		
+	}
 
 	public String getNom() {
 		return nom;
