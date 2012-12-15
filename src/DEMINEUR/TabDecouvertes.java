aa26c23508e5419b6713d2135b53c8f546320914
Cédric <ced@MacBook-Pro-de-Ced.local>
2012-12-15 01:27:47 +0100

    Revert "Revert "Decouvrir""
    This reverts commit 84e7fffe9dede5a8359d1fb47030fb58b7e23e9b.

--- src/DEMINEUR/TabDecouvertes.java
+++ src/DEMINEUR/TabDecouvertes.java
@@ -3,32 +3,41 @@ package DEMINEUR;
 import GUI.Fenetre;
 
 public class TabDecouvertes {
-	
+
 	private static boolean[][] decouverte = new boolean[Fenetre.getNbLignes()][Fenetre.getNbCols()];
-	
+
 	public TabDecouvertes() {
-		
+
 		initDecouvertes();
-		
+
 	}
 
 	private void initDecouvertes() {
-		
+
 		for (int i = 0; i < Fenetre.getNbLignes() ; i++) {
 			for(int j = 0; j < Fenetre.getNbCols() ; j++) {
 				decouverte[i][j] = false;
 			}
 		}
-		
+
+	}
+
+	public void decouvrirTab() {
+
+		for (int i = 0; i < Fenetre.getNbLignes() ; i++) {
+			for(int j = 0; j < Fenetre.getNbCols() ; j++) {
+
+			}
+		}
+
 	}
 
 	public static boolean getDecouverte(int x, int y) {
 		return decouverte[x][y];
 	}
 
-	public static void setDecouverte(boolean decouverte, int x, int y) {
-		TabDecouvertes.decouverte[x][y] = decouverte;
+	public static void setDecouverte(boolean value, int x, int y) {
+		decouverte[x][y] = value;
 	}
-	
-	
+
 }
