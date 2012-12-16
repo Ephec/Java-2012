8f70181ad94a6d0277330ed1abd6d53b516c22fc
Antoine Betas <antoine.betas@gmail.com>
2012-12-16 19:23:43 +0100

    Ajout des package-info
    Je sais pas si c'est ok mais bon au moins c'est tenté

---
+++ src/DEMINEUR/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author BETAS A. & BREMER C.
+ * @version 1.0
+ * @category Code
+ * 
+ * Ce package s'occupe de la gestion du code du démineur.
+ * Les 4 classes tab... créer les tableaux qui gèrent les états des cases.
+ * Le Chrono contient le thread du chronomètre et le Score gère les scores des joueurs
+ * 
+ */
+package DEMINEUR;
---
+++ src/GUI/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author BETAS A. & BREMER C.
+ * @version 1.0
+ * @category Graphique
+ * 
+ * Ce package s'occupe de la gestion graphique du démineur
+ * La Fenetre contrôle tout le reste
+ * Niveau permet de sélectionner le niveau de jeu
+ * NiveauPerso permet de sélectionner les lignes, colonnes et mines
+ * ScoreVue affiche les scores enregistrés
+ */
+package GUI;
