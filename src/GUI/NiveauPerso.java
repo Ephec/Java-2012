d583f33847cd89ec8784aa685e9ee68ed9b73530
Cédric <ced@MacBook-Pro-de-Ced.local>
2012-12-16 17:39:53 +0100

    Modif nivPerso (bug des lettres)

--- src/GUI/NiveauPerso.java
+++ src/GUI/NiveauPerso.java
@@ -12,32 +12,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NiveauPerso extends JFrame{
-	
+
 	private JPanel boite = new JPanel();
-	
+
 	private JLabel txtLignes = new JLabel("Nombre de lignes  : ");
 	private JLabel txtCols = new JLabel("Nombre de colonnes : ");
 	private JLabel txtMines = new JLabel("Nombre de mines   : ");
-	
+
 	private JTextField nbLignesC = new JTextField();
 	private JTextField nbColsC = new JTextField();
 	private JTextField nbMinesC = new JTextField();
-	
+
 	private JButton ok = new JButton("ok");
 	private JButton annuler = new JButton("Annuler");
-	
+
 	private int nbLignes;
 	private int nbCols;
 	private int nbMines;
-	
+
 	public NiveauPerso() {
 
 		initNivPerso();
 
 	}
-	
+
 	public void initNivPerso(){
-		
+
 		this.setTitle("Niveau personnalisé");
 		this.setMinimumSize(new Dimension(300, 200));
 		this.setLocationRelativeTo(null);
@@ -45,17 +45,17 @@ public class NiveauPerso extends JFrame{
 
 		this.add(boite);
 		boite.setLayout(new GridLayout(5,2));
-	
+
 		boite.add(txtLignes);
 		boite.add(nbLignesC);
 		boite.add(txtCols);
 		boite.add(nbColsC);
 		boite.add(txtMines);
 		boite.add(nbMinesC);
-		
+
 		boite.add(new JLabel());
 		boite.add(new JLabel());
-		
+
 		annuler.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				Fenetre f = new Fenetre();
@@ -63,28 +63,32 @@ public class NiveauPerso extends JFrame{
 			}        
 		});
 		boite.add(annuler);
-		
+
 		ok.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				if(verifEntrees()){
 					creationNiv();
 					dispose();
 				}else{
-					JOptionPane.showMessageDialog(null, "Saisie non correcte ! \n- Max 50 lignes ou colonnes \n- Min 5 lignes ou colonnes \n- Pas plus de mines que de cases", "Erreur", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(null, "Saisie non correcte ! \n\n- Max 50 lignes ou colonnes \n- Min 5 lignes ou colonnes \n- Pas plus de mines que de cases \n- Que des chiffres ou nombres", "Erreur", JOptionPane.ERROR_MESSAGE);
 				}
-				
+
 			}        
 		});
 		boite.add(ok);
 
 		this.setVisible(true);
 	}
-	
+
 	public boolean verifEntrees(){
-		
-		nbLignes = Integer.parseInt(nbLignesC.getText());
-		nbCols = Integer.parseInt(nbColsC.getText());
-		nbMines = Integer.parseInt(nbMinesC.getText());
+
+		try{
+			nbLignes = Integer.parseInt(nbLignesC.getText());
+			nbCols = Integer.parseInt(nbColsC.getText());
+			nbMines = Integer.parseInt(nbMinesC.getText());
+		} catch (NumberFormatException n) {
+			return false;
+		}
 		
 		if(nbMines < nbLignes * nbCols && nbLignes <= 50 && nbLignes >= 5 && nbCols <= 50 && nbCols >= 5){
 			return true;
@@ -92,14 +96,14 @@ public class NiveauPerso extends JFrame{
 			return false;
 		}
 	}
-	
+
 	public void creationNiv() {
-		
+
 		Fenetre.setNbLignes(nbLignes);
 		Fenetre.setNbCols(nbCols);
 		Fenetre.setNbMines(nbMines);
 		Fenetre f = new Fenetre();
-		
+
 	}
 
 }
