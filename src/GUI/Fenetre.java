bd83e519f9bacbed2ee8a47288fb38f5c29313ee
Antoine Betas <antoine.betas@gmail.com>
2012-12-16 18:42:41 +0100

    javadoc - étape 3
    Fenetre

--- src/GUI/Fenetre.java
+++ src/GUI/Fenetre.java
@@ -28,8 +28,8 @@ import DEMINEUR.TabProxi;
  * 
  * @author BETAS A. & BREMER C.
  * 
- * Classe générale de la fenêtre de jeu
- *
+ *         Classe générale de la fenêtre de jeu
+ * 
  */
 public class Fenetre extends JFrame implements MouseListener {
 
@@ -79,9 +79,9 @@ public class Fenetre extends JFrame implements MouseListener {
 	static Chrono chrono = new Chrono();
 
 	/**
-	 * 
+	 * Constructeur de la fenêtre appelé dans la class Main
 	 */
-	public Fenetre(){
+	public Fenetre() {
 
 		this.init();
 		this.setMenu();
@@ -96,7 +96,7 @@ public class Fenetre extends JFrame implements MouseListener {
 	/**
 	 * Initialisation de la fenêtre
 	 */
-	public void init(){
+	public void init() {
 
 		this.setTitle("Démineur en Java");
 		this.setMinimumSize(new Dimension(400, 400));
@@ -106,30 +106,31 @@ public class Fenetre extends JFrame implements MouseListener {
 		details.setEditable(false);
 
 	}
-	/*
-	 * barre de menu
+
+	/**
+	 * Génère la barre des menus et ses actions
 	 */
 	public void setMenu() {
 
 		// Menu "Partie"
-		nouvelle.addActionListener(new ActionListener(){
+		nouvelle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Niveau niveau = new Niveau();
 				nivActuel = niveau.getReponse();
 				setNiveau(nivActuel);
-			}        
+			}
 		});
 		partie.add(nouvelle);
 		partie.add(reseau);
-		statistiques.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		statistiques.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				ScoresVue scoresVue = new ScoresVue();
 			}
 		});
 		partie.add(statistiques);
 		partie.addSeparator();
 
-		fermer.addActionListener(new ActionListener(){
+		fermer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
@@ -137,21 +138,24 @@ public class Fenetre extends JFrame implements MouseListener {
 		partie.add(fermer);
 
 		// Menu "?"
-		aide.addActionListener(new ActionListener(){
+		aide.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Cliquez sur les cases sans toucher les mines ...");
-			}        
+				JOptionPane.showMessageDialog(null,
+						"Cliquez sur les cases sans toucher les mines ...");
+			}
 		});
 		infos.add(aide);
 
-		apropos.addActionListener(new ActionListener(){
+		apropos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Démineur\nVersion : décembre 2012\nBy Antoine Betas & Cédric Bremer");
-			}        
+				JOptionPane
+						.showMessageDialog(null,
+								"Démineur\nVersion : décembre 2012\nBy Antoine Betas & Cédric Bremer");
+			}
 		});
 		infos.add(apropos);
 
-		// Barre générale qui contient les différents menu 
+		// Barre générale qui contient les différents menu
 		partie.setMnemonic('P');
 		menuBar.add(partie);
 		infos.setMnemonic('?');
@@ -163,12 +167,13 @@ public class Fenetre extends JFrame implements MouseListener {
 	/**
 	 * @param niveau
 	 * 
-	 * Un switch qui en fonction du nombre entier determine le niveau. Ce niveau
-	 * sera passé en paramètre à la méthode qui créer le plateau. De plus, cette
-	 * méthode retournera le niveau de la partie à la classe mère, Démineur ce
-	 * qui permettra de générer le niveau pour les scores
+	 *            Un switch qui en fonction du nombre entier determine le
+	 *            niveau. Ce niveau sera passé en paramètre à la méthode qui
+	 *            créer le plateau. De plus, cette méthode retournera le niveau
+	 *            de la partie à la classe mère, Démineur ce qui permettra de
+	 *            générer le niveau pour les scores
 	 */
-	public void setNiveau(int niveau){
+	public void setNiveau(int niveau) {
 
 		// On set le niveau, la prochaine étape est donc le premier clic
 		switch (niveau) {
@@ -191,10 +196,10 @@ public class Fenetre extends JFrame implements MouseListener {
 			this.nbMinesRest = nbMines;
 			break;
 		case 3:
-			//System.out.println(""+nbLignes);
+			// System.out.println(""+nbLignes);
 			niveauPerso = new NiveauPerso();
 			dispose();
-			//System.out.println(""+nbLignes);
+			// System.out.println(""+nbLignes);
 		}
 		setGrille();
 
@@ -206,10 +211,10 @@ public class Fenetre extends JFrame implements MouseListener {
 	 * @param lignes
 	 * @param cols
 	 */
-	public void setGrille(){
+	public void setGrille() {
 
 		this.remove(container);
-		premierClic=true;
+		premierClic = true;
 
 		container = new JPanel();
 		btnCase = new JButton[nbLignes][nbCols];
@@ -219,72 +224,84 @@ public class Fenetre extends JFrame implements MouseListener {
 		decouvertes = new TabDecouvertes();
 		drapeaux = new TabDrapeaux();
 
-		//mines.initMines(nbMines, nbLignes, nbCols);
-		//nbre.nbMinesCase(lignes, cols);
-		//decouvertes.initDecouvertes(nbLignes, nbCols);
+		// mines.initMines(nbMines, nbLignes, nbCols);
+		// nbre.nbMinesCase(lignes, cols);
+		// decouvertes.initDecouvertes(nbLignes, nbCols);
 
 		container.setLayout(new GridLayout(nbLignes, nbCols));
 
-		for (int i = 0; i < nbLignes ; i++) {
-			for(int j = 0; j < nbCols ; j++) {
+		for (int i = 0; i < nbLignes; i++) {
+			for (int j = 0; j < nbCols; j++) {
 				btnCase[i][j] = new JButton();
 				btnCase[i][j].setEnabled(true);
-				btnCase[i][j].setPreferredSize(new Dimension(45,45));
+				btnCase[i][j].setPreferredSize(new Dimension(45, 45));
 				btnCase[i][j].addMouseListener(this);
 				container.add(btnCase[i][j]);
 
 			}
 		}
 
-		details.setText("\n  Lignes : "+nbLignes +" \n  Colonnes : "+nbCols+" \n  Mines : "+nbMinesRest+" \n");
+		details.setText("\n  Lignes : " + nbLignes + " \n  Colonnes : "
+				+ nbCols + " \n  Mines : " + nbMinesRest + " \n");
 		this.add(container);
 		this.setLocationRelativeTo(null);
 		this.pack();
 
 	}
 
+	/**
+	 * Vérifie si une partie est gagnée si le nombre de cases non découvertes
+	 * correspond au nombre mines restantes
+	 */
 	public void verifGagne() {
 
 		int compteur = 0;
 		int nbNonDecouvertes = 0;
 
-		for (int i = 0; i < nbLignes ; i++) {
-			for(int j = 0; j < nbCols ; j++) {
-				if(!decouvertes.getDecouverte(i, j)){
+		for (int i = 0; i < nbLignes; i++) {
+			for (int j = 0; j < nbCols; j++) {
+				if (!decouvertes.getDecouverte(i, j)) {
 					nbNonDecouvertes++;
 				}
 			}
 		}
-		if(nbNonDecouvertes == nbMines){
-			for (int i = 0; i < nbLignes ; i++) {
-				for(int j = 0; j < nbCols ; j++) {
-					if(mines.getMine(i, j) && !decouvertes.getDecouverte(i, j)){
+		if (nbNonDecouvertes == nbMines) {
+			for (int i = 0; i < nbLignes; i++) {
+				for (int j = 0; j < nbCols; j++) {
+					if (mines.getMine(i, j) && !decouvertes.getDecouverte(i, j)) {
 						compteur++;
 					}
 				}
 			}
 		}
 
-		if(compteur == nbMines){
+		if (compteur == nbMines) {
 
-			int reponse = - 1;
+			int reponse = -1;
 			chrono.stop();
-			String[] action = {"Recommencer", "Quitter"};
+			String[] action = { "Recommencer", "Quitter" };
 
-			String nom = (String) JOptionPane.showInputDialog(null, "Félicitations, vous avez gagné en "+chrono.resultat()+" secondes... \n\nQuel est votre nom ?", "Partie gagnée", JOptionPane.QUESTION_MESSAGE,null,null,getComputerFullName());
-			
-			if(nom!=null){
-				//System.out.println(""+nivActuel);
+			String nom = (String) JOptionPane.showInputDialog(null,
+					"Félicitations, vous avez gagné en " + chrono.resultat()
+							+ " secondes... \n\nQuel est votre nom ?",
+					"Partie gagnée", JOptionPane.QUESTION_MESSAGE, null, null,
+					getComputerFullName());
+
+			if (nom != null) {
+				// System.out.println(""+nivActuel);
 				scores = new Scores(nom, nivActuel, chrono.resultat());
 				scores.ecrireFichier();
 			}
 
-			reponse = JOptionPane.showOptionDialog(null, "Que voulez-vous faire ?", "Partie gagnée", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, action, action[1]);
+			reponse = JOptionPane.showOptionDialog(null,
+					"Que voulez-vous faire ?", "Partie gagnée",
+					JOptionPane.YES_NO_CANCEL_OPTION,
+					JOptionPane.QUESTION_MESSAGE, null, action, action[1]);
 
-			if(reponse == 1){
+			if (reponse == 1) {
 				System.exit(0);
 			}
-			if(reponse == 0){
+			if (reponse == 0) {
 				Niveau niveau = new Niveau();
 				setNiveau(niveau.getReponse());
 			}
@@ -294,66 +311,90 @@ public class Fenetre extends JFrame implements MouseListener {
 
 	/**
 	 * Fonction permettant de retourner le nom de la machine
-	 * @return
+	 * 
+	 * @return String
 	 */
 	public static String getComputerFullName() {
 		String hostName = null;
 		try {
 			final InetAddress addr = InetAddress.getLocalHost();
 			hostName = new String(addr.getHostName());
-		} catch(final Exception e) {
+		} catch (final Exception e) {
 		}
 		return hostName;
 	}
 
+	/**
+	 * Partie perdu si clic sur une mine, genère une fenêtre pour recommencer
+	 * une partie ou pour quitter le jeu
+	 */
 	public void partiePerdue() {
 
 		int reponse = -1;
 		chrono.stop();
-		
-		String[] action = {"Recommencer", "Quitter"};
-		while(reponse == -1){
+
+		String[] action = { "Recommencer", "Quitter" };
+		while (reponse == -1) {
 			decouvrirMines();
-			reponse = JOptionPane.showOptionDialog(null, "Vous avez malheureusement explosé sur un mine ...", "Partie perdue", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, action, action[1]);
+			reponse = JOptionPane.showOptionDialog(null,
+					"Vous avez malheureusement explosé sur un mine ...",
+					"Partie perdue", JOptionPane.YES_NO_CANCEL_OPTION,
+					JOptionPane.QUESTION_MESSAGE, null, action, action[1]);
 		}
 
-		if(reponse == 1){
+		if (reponse == 1) {
 			System.exit(0);
 		}
-		if(reponse == 0){
+		if (reponse == 0) {
 			Niveau niveau = new Niveau();
 			setNiveau(niveau.getReponse());
 		}
 	}
 
-	public void decouvrirCase(int x, int y){
-		decouvertes.setDecouverte(true,x,y);
-		btnCase[x][y].setBackground(new java.awt.Color(190,190,190));
-		btnCase[x][y].setForeground(new java.awt.Color(0,0,0));
+	/**
+	 * Permet de découvrir une case en jouant sur le Enable du JButton et sur
+	 * ses couleurs. Affiche également le nombre de mines à proximité
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void decouvrirCase(int x, int y) {
+		decouvertes.setDecouverte(true, x, y);
+		btnCase[x][y].setBackground(new java.awt.Color(190, 190, 190));
+		btnCase[x][y].setForeground(new java.awt.Color(0, 0, 0));
 		btnCase[x][y].setEnabled(false);
-		if(nbre.getNbre(x, y)!=0) btnCase[x][y].setText(""+nbre.getNbre(x,y));
+		if (nbre.getNbre(x, y) != 0)
+			btnCase[x][y].setText("" + nbre.getNbre(x, y));
 
 	}
 
-	public void decouvrirAutour(int x, int y){
+	/**
+	 * Decouvre les cases autour d'une case cliquée si cette dernière ne
+	 * contient pas de mine à proximité
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void decouvrirAutour(int x, int y) {
 
-		for(int i=-1;i<=1;i++){
-			for(int j=-1;j<=1;j++){
-				if((x+i)>= 0 && (x+i)<nbLignes && (y+j)>=0 && (y+j)<nbCols){
-					if(
-							nbre.getNbre(x+i, y+j) == 0 && !decouvertes.getDecouverte(x+i, y+j)
-							)
-					{
-						if(!drapeaux.getDrapeau(x+i, y+j)) {
-							decouvrirCase(x+i,y+j);
-							decouvrirAutour(x+i,y+j);
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if ((x + i) >= 0 && (x + i) < nbLignes && (y + j) >= 0
+						&& (y + j) < nbCols) {
+					if (nbre.getNbre(x + i, y + j) == 0
+							&& !decouvertes.getDecouverte(x + i, y + j)) {
+						if (!drapeaux.getDrapeau(x + i, y + j)) {
+							decouvrirCase(x + i, y + j);
+							decouvrirAutour(x + i, y + j);
 						}
-						//else decouvrirAutour(x+i,y+j);
+						// else decouvrirAutour(x+i,y+j);
 					}
 
-					else if(!decouvertes.getDecouverte(x+i, y+j)){
-						//btnCase[x+i][y+j].setText(String.valueOf(nbre.getNbre(x+i, y+j)));
-						if (!drapeaux.getDrapeau(x+i, y+j)) decouvrirCase(x+i,y+j);
+					else if (!decouvertes.getDecouverte(x + i, y + j)) {
+						// btnCase[x+i][y+j].setText(String.valueOf(nbre.getNbre(x+i,
+						// y+j)));
+						if (!drapeaux.getDrapeau(x + i, y + j))
+							decouvrirCase(x + i, y + j);
 					}
 				}
 			}
@@ -361,72 +402,94 @@ public class Fenetre extends JFrame implements MouseListener {
 
 	}
 
-	public void decouvrirMines(){
-		for (int i=0;i<nbLignes;i++){
-			for(int j=0;j<nbCols;j++){
-				if(mines.getMine(i,j)){
+	/**
+	 * Découvre toutes les mines d'un plateau en cas de défaite
+	 */
+	public void decouvrirMines() {
+		for (int i = 0; i < nbLignes; i++) {
+			for (int j = 0; j < nbCols; j++) {
+				if (mines.getMine(i, j)) {
 					btnCase[i][j].setEnabled(false);
-					if (drapeaux.getDrapeau(i,j))
-						btnCase[i][j].setBackground(new java.awt.Color(0,255,0));
+					if (drapeaux.getDrapeau(i, j))
+						btnCase[i][j].setBackground(new java.awt.Color(0, 255,
+								0));
 					else
-						btnCase[i][j].setBackground(new java.awt.Color(255,0,0));
+						btnCase[i][j].setBackground(new java.awt.Color(255, 0,
+								0));
 					btnCase[i][j].setText("M");
 				}
 			}
 		}
 	}
 
+	/**
+	 * MouseListener qui gère les clics et genère l'action en fonction du clic
+	 */
 	public void mousePressed(MouseEvent e) {
 		// recupère le clic (gauche ou droite)
 		int clic = e.getButton();
-		
-		if(premierClic){
+
+		if (premierClic) {
 			chrono.reset();
 			chrono.start();
-			premierClic=false;
+			premierClic = false;
 		}
 
-		for(int i = 0; i < nbLignes; i++){
-			for(int j = 0; j < nbCols; j++){
-				if(e.getSource() == btnCase[i][j]){  // lie le clic à une case avec ses coordonnées
-					if(clic == 1){ // si clic gauche
-						if(!drapeaux.getDrapeau(i, j)){
-							if(mines.getMine(i,j)){  // regarde si c'est une mine
+		for (int i = 0; i < nbLignes; i++) {
+			for (int j = 0; j < nbCols; j++) {
+				if (e.getSource() == btnCase[i][j]) { // lie le clic à une case
+														// avec ses coordonnées
+					if (clic == 1) { // si clic gauche
+						if (!drapeaux.getDrapeau(i, j)) {
+							if (mines.getMine(i, j)) { // regarde si c'est une
+														// mine
 								partiePerdue();
 								break;
-							}else{ // si non, indique les mines au alentours
+							} else { // si non, indique les mines au alentours
 
-								int nbProxi = nbre.getNbre(i,j);
+								int nbProxi = nbre.getNbre(i, j);
 
-								if(nbProxi == 0){
+								if (nbProxi == 0) {
 									btnCase[i][j].setText("");
-									//decouvrirCase(i,j);
-									decouvrirAutour(i,j);
-
+									// decouvrirCase(i,j);
+									decouvrirAutour(i, j);
 
 								} else {
-									decouvrirCase(i,j);
-									//btnCase[i][j].setText(""+nbre.getNbre(i,j));
+									decouvrirCase(i, j);
+									// btnCase[i][j].setText(""+nbre.getNbre(i,j));
 								}
 
 								verifGagne();
 
 							}
-							//btnCase[i][j].setEnabled(false);  // désactive les cases cliquée mais ca ne désactive pas complètement :p
+							// btnCase[i][j].setEnabled(false); // désactive les
+							// cases cliquée mais ca ne désactive pas
+							// complètement :p
 						}
 					}
-					if(clic == 3 && !decouvertes.getDecouverte(i,j)) {  // si clic droit, ajout d'un drapeau
-						if(!drapeaux.getDrapeau(i,j)){
-							btnCase[i][j].setText("D"); //faut une solution car on peut cliquer plusieurs fois sur une meme case et meme si elle est disable :p
+					if (clic == 3 && !decouvertes.getDecouverte(i, j)) { // si
+																			// clic
+																			// droit,
+																			// ajout
+																			// d'un
+																			// drapeau
+						if (!drapeaux.getDrapeau(i, j)) {
+							btnCase[i][j].setText("D"); // faut une solution car
+														// on peut cliquer
+														// plusieurs fois sur
+														// une meme case et meme
+														// si elle est disable
+														// :p
 							drapeaux.setDrapeau(true, i, j);
-							nbMinesRest --;
-						}
-						else {
+							nbMinesRest--;
+						} else {
 							btnCase[i][j].setText("");
 							drapeaux.setDrapeau(false, i, j);
-							nbMinesRest ++;
+							nbMinesRest++;
 						}
-						details.setText("\n  Lignes : "+nbLignes +" \n  Colonnes : "+nbCols+" \n  Mines : "+nbMinesRest+"\n");
+						details.setText("\n  Lignes : " + nbLignes
+								+ " \n  Colonnes : " + nbCols + " \n  Mines : "
+								+ nbMinesRest + "\n");
 					}
 				}
 			}
@@ -436,7 +499,6 @@ public class Fenetre extends JFrame implements MouseListener {
 	public void mouseClicked(MouseEvent e) {
 
 	}
-
 
 	public void mouseReleased(MouseEvent e) {
 
@@ -477,7 +539,4 @@ public class Fenetre extends JFrame implements MouseListener {
 		Fenetre.nbMinesRest = nbMines;
 	}
 
-
-
 }
-
